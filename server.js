43802c9f5e2771ead165ef12023a2453115b6cbc
orwel009 <orwelvincent1@gmail.com>
2025-10-16 14:45:15 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
